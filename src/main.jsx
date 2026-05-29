72b03e648f0285423fc03bda4550ce83e1e34d57
Jonathankohlanyj <138862358+Jonathankohlanyj@users.noreply.github.com>
2026-05-29 22:58:57 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import "./style.css";
 
-const root = document.getElementById("root");
-
-if (!root) {
-  document.body.innerHTML = "<h1 style='color:red'>Root element not found</h1>";
-} else {
-  createRoot(root).render(<App />);
-}
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
