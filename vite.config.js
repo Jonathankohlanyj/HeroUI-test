5ca9bc36bc323c2776a769dd5d97d062bbe64f61
Jonathankohlanyj <138862358+Jonathankohlanyj@users.noreply.github.com>
2026-05-30 18:40:48 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { heroui } from "@heroui/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    heroui()
+  ],
   base: "./"
 });
